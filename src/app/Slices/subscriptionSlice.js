c304b3e551c88f16403f976777de3c8a403dd8f6
Ritik  Kumar <rsengar0089@gmail.com>
2024-09-02 00:06:49 +0530

    Added some App Slices and installed some packages

---
+++ src/app/Slices/subscriptionSlice.js
@@ -0,0 +1,88 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { parseErrorMessage } from "../../helpers/parseErrMsg.helper";
+import { axiosInstance } from "../../helpers/axios.helper";
+import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
+
+const initialState = {
+  loading: false,
+  status: false,
+  data: null,
+};
+
+export const toggleSubscription = createAsyncThunk("subscription/toggleSubscription", async (channelId) => {
+    try {
+      const response = await axiosInstance.post(`/subscriptions/c/${channelId}`);
+      toast.success(response.data.message);
+      return response.data.data;
+    } 
+    catch (error) {
+      toast.error(parseErrorMessage(error.response.data));
+      console.log(error);
+    }
+});
+
+
+export const getChannelSubscribers = createAsyncThunk("subscription/getChannelSubscribers", async (channelId) => {
+    try {
+      const response = await axiosInstance.get(`/subscriptions/u/${channelId}`);
+      return response.data.data;
+    } 
+    catch (error) {
+      toast.error(parseErrorMessage(error.response.data));
+      console.log(error);
+    }
+});
+
+
+export const getSubscribedChannels = createAsyncThunk("subscription/getSubscribedChannels", async (subscriberId) => {
+    try {
+      const response = await axiosInstance.get(`/subscriptions/c/${subscriberId}`);
+      return response.data.data;
+    }
+    catch (error) {
+      toast.error(parseErrorMessage(error.response.data));
+      console.log(error);
+    }
+});
+
+
+const subscriptionSlice = createSlice({
+  name: "subscription",
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(toggleSubscription.pending, (state) => {});
+    builder.addCase(toggleSubscription.fulfilled, (state,action) => {});
+    builder.addCase(toggleSubscription.rejected, (state) => {});
+
+    // get Channel Subscribers
+    builder.addCase(getChannelSubscribers.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(getChannelSubscribers.fulfilled, (state, action) => {
+      state.loading = false;
+      state.data = action.payload;
+      state.status = true;
+    });
+    builder.addCase(getChannelSubscribers.rejected, (state) => {
+      state.loading = false;
+      state.status = false;
+    });
+
+    // get Subscribed Channels
+    builder.addCase(getSubscribedChannels.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(getSubscribedChannels.fulfilled, (state, action) => {
+      state.loading = false;
+      state.data = action.payload;
+      state.status = true;
+    });
+    builder.addCase(getSubscribedChannels.rejected, (state) => {
+      state.loading = false;
+      state.status = false;
+    });
+  },
+});
+
+export default subscriptionSlice.reducer;
