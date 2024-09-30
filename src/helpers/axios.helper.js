d4d4d1ea4524cb830a4140f605f2ec9621870629
Ritik  Kumar <rsengar0089@gmail.com>
2024-10-01 01:22:34 +0530

    updated Base URL

--- src/helpers/axios.helper.js
+++ src/helpers/axios.helper.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const baseURL = "https://twido-backend.onrender.com//api/v1";
+const baseURL = "https://twido-backend.onrender.com/api/v1";
 
 export const axiosInstance = axios.create({
   baseURL,
