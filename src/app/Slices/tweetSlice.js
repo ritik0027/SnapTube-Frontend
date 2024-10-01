5598d3389c3bc36d9b5949334b56ef67128c4107
Ritik  Kumar <rsengar0089@gmail.com>
2024-10-01 23:28:39 +0530

    Fixed Some Tweet bugs

--- src/app/Slices/tweetSlice.js
+++ src/app/Slices/tweetSlice.js
@@ -7,7 +7,7 @@ import { toast } from "react-toastify";
 const initialState = {
   loading: false,
   status: false,
-  data: [],
+  data: null,
 };
 
 export const createTweet = createAsyncThunk("tweet/createTweet", async ( content ) => {
@@ -42,7 +42,6 @@ export const getAllTweets = createAsyncThunk("tweet/getAllTweets", async () => {
   try {
     const response = await axiosInstance.get(`/tweets`);
     //toast.success(response.data.message);
-    console.log(response.data.data)
     return response.data.data;
   } catch (error) {
     toast.error(parseErrorMessage(error.response.data));
@@ -50,19 +49,6 @@ export const getAllTweets = createAsyncThunk("tweet/getAllTweets", async () => {
     throw error;
   }
 });
-
-
-// export const getAllUserFeedTweets = createAsyncThunk("tweet/getAllUserFeedTweets", async () => {
-//   try {
-//     const response = await axiosInstance.get(`/tweets/feed`);
-//     //toast.success(response.data.message);
-//     return response.data.data;
-//   } catch (error) {
-//     toast.error(parseErrorMessage(error.response.data));
-//     console.log(error);
-//     throw error;
-//   }
-// });
 
 
 export const updateTweet = createAsyncThunk("tweet/updateTweet", async ({ tweetId, data }) => {
@@ -101,11 +87,7 @@ const tweetSlice = createSlice({
     });
     builder.addCase(createTweet.fulfilled, (state, action) => {
       state.loading = false;
-      if (!Array.isArray(state.data)) {
-        state.data = []; // Ensure data is an array
-      }
       state.data.unshift(action.payload);
-      console.log("")
       state.status = true;
     });
     builder.addCase(createTweet.rejected, (state) => {
@@ -127,21 +109,19 @@ const tweetSlice = createSlice({
       state.status = false;
     });
     
-    //get All User Feed Tweets
+    //get All tweets
     builder.addCase(getAllTweets.pending, (state) => {
       state.loading = true;
     });
     builder.addCase(getAllTweets.fulfilled, (state, action) => {
       state.loading = false;
       state.data = action.payload;
-      console.log("state data in get req",state.data)
       state.status = true;
     });
     builder.addCase(getAllTweets.rejected, (state) => {
       state.loading = false;
       state.status = false;
     });
-
 
     // Update tweet
     builder.addCase(updateTweet.pending, (state) => {
@@ -163,16 +143,8 @@ const tweetSlice = createSlice({
     });
     builder.addCase(deleteTweet.fulfilled, (state, action) => {
       state.loading = false;
-      console.log("delete tweet:",action.payload);
-      console.log("delete tweet state:",state);
-      // state.loading = false;
-
-      // Ensure the state's data isn't empty before filtering
-      if (state.data && state.data.length > 0) {
-        let filteredTweets = state.data.filter((tweet) => tweet._id !== action.payload._id);
-        state.data = filteredTweets;
-      }
-    
+      let filteredTweets = state.data.filter((tweet) => tweet._id !== action.payload._id);
+      state.data = filteredTweets;
       state.status = true;
     });
     builder.addCase(deleteTweet.rejected, (state) => {
